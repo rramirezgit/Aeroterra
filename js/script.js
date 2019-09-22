a20ea2448f40356f006071b25b586b490a4919b9
Ricardo <97.rramirez@gmail.com>
2019-09-22 16:41:48 -0300

    Agregar un punto por coodernadas
    Se captura el valor de la coordenada desde el formulario y se marca un punto en el mapa

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
+function addPoint(){
+	var coordenadas = document.getElementById('inputCoordenadas').value;
+	coordenadas = coordenadas.split(",",2);
+	var latitud = coordenadas[0];
+	var longitud = coordenadas[1];
+
+	latitud = parseFloat(latitud);
+	longitud = parseFloat(longitud);
+
+	L.control.scale().addTo(map);
+	L.marker([latitud,longitud], {draggable: false}).addTo(map);
+}
+
 var map = L.map('map').
-setView([-34.6174957,-58.4106985,], 
+setView([-34.6162486,-58.4360359], 
 12);
  
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 }).addTo(map);
-
-L.control.scale().addTo(map);
-L.marker([41.66, -4.71], {draggable: true}).addTo(map);
-
